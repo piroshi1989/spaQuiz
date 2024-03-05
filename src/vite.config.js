36dbbe27723abcb7e937b6f54a0f9b0cad90323e
piroshi1989 <hiroshi19890606@gmail.com>
2024-03-06 08:45:24 +0900

    ts設定

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     server: {
@@ -16,4 +17,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+        }
+      }
 });
