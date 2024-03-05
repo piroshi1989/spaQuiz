df98111538d291faf699186a64519ad951d70ec7
piroshi1989 <hiroshi19890606@gmail.com>
2024-03-05 22:46:35 +0900

    first-commit

---
+++ src/resources/js/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0)
+const addCount = () => {
+    count.value++
+}
+
+</script>
+
+<template>
+    <button @click="addCount">{{ count }} 回クリックしましたよ！</button>
+</template>
